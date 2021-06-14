d86deaa624fb435c7359edaa023e41d50a072b76
Ye Shi <zoe.shiye@gmail.com>
2021-06-14 12:06:29 +1000

    Add: Sign Up page

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res)=>{
+  res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email)
+})
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
